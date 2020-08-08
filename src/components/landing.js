354917302dcd5a6979558cb9c6345db3ee1c898f
GOCE1982 <goce.ilievski@live.com>
2020-08-08 14:09:01 +0200

    Landing Page fix

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Cell, List, ListItem, ListItemContent, Button } from 'react-mdl';
+import { Link } from 'react-router-dom';
+import { Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import profile from "../img/img.jpg";
@@ -12,27 +13,47 @@ class Landing extends Component {
                     <Cell col={12} style={{paddingTop: '3.5em', margin: 0}} className="dark-overlay">
                             <img src={profile} alt="avatar" className="avatar-image" />
                             <p></p>
-                            <h4 className="banner-text">Fullstack JavaScript Web Developer</h4>
+                            <h1 className="banner-text">Fullstack Web Developer</h1>
+                    </Cell>
+                    <Cell col={12} className="dark-overlay" style={{margin: 0}}>
                         <div style={{display: 'flex', justifyContent: 'center'}}>
                             <List>
                                 <ListItem >
                                     <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>Highly Motivated</ListItemContent>
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>Fully Commited</ListItemContent>
+                                    <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>Fully Reliable</ListItemContent>
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>Team Player</ListItemContent>
+                                    <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>JavaScript Expert</ListItemContent>
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>Big Tech Nerd</ListItemContent>
+                                    <ListItemContent className="list-item-cont"><FontAwesomeIcon icon={faStar} pull="left"/>Tech Nerd</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent className="list-item-cont">
+                                        <Link to={'/projects'}>View My Projects</Link>
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent className="list-item-cont">
+                                        <div className="social-links">
+                                            <Link target="_blank" to="//linkedin.com/in/goce-ilievski-06b863132/">
+                                                <i className="fa fa-linkedin-square" area-hidden="true" />
+                                            </Link>
+                                            
+                                            <Link target="_blank" to="//github.com/GOCE1982">
+                                                <i className="fa fa-github-square" area-hidden="true" />
+                                            </Link>
+                                        </div>
+                                    </ListItemContent>
                                 </ListItem>
                             </List>
                         </div>
                     </Cell>
                     <Cell col={12}>
-                        <div className="page-content">
-                            <Button raised colored ripple href="/projects">Browse My Projects</Button>
+                        <div className="banner-text">
+                            <h6>HTML/CSS | JavaScript | React | Redux | NodeJS | Express | MongoDB</h6>
                         </div>
                     </Cell>
                 </Grid> 
