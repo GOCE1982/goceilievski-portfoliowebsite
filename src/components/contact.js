b87e1686ecd469e5fbe4d4e19f0e8c004a7ffb6d
GOCE1982 <goce.ilievski@live.com>
2020-09-21 16:15:36 +0200

    Contact form add

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
-import { Grid, Cell, List, ListItemContent, ListItem } from 'react-mdl';
+import { Grid,
+         Cell,
+         List,
+         ListItemContent,
+         ListItem,
+         Textfield,
+         Button
+         } from 'react-mdl';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsappSquare, faViber, faSkype, faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 class Contact extends Component {
+    state = {
+        success: false
+    }
+
+    setSuccess = () => {
+        if(window.location.search.includes('success=true')) {
+            this.setState({success: true})
+        }
+    }
+
     render() {
         return (
             <div className="contact-body">
                 <Grid className="contact-grid">
-                    
+                    <Cell col={6}>
+                        <h3>Send me a message</h3>
+                        {this.state.success && (
+                            <p style={{color: 'green'}}>Successfully sent message</p>
+                        )}
+                        <form name="contact" method="POST" data-netlify="true" action="/?success=true">
+                            <input type="hidden" name="form-name" value="contact" />
+                            <p>
+                                <Textfield 
+                                    onChange={() => {}}
+                                    label="Name"
+                                    style={{width: '200px'}}
+                                />
+                            </p>
+                            <p>
+                                <Textfield 
+                                    onChange={() => {}}
+                                    label="Email"
+                                    style={{width: '200px'}}
+                                />
+                            </p>
+                            <p>
+                                <Textfield 
+                                    onChange={() => {}}
+                                    label="Message"
+                                    floatingLabel
+                                    rows="8"
+                                    maxRows="12"
+                                />
+                            </p>
+                            <p>
+                                <Button type="submit">Send</Button>
+                            </p>
+                        </form>
+                    </Cell>
                     <Cell col={6}>
                         <h2>Contact Me</h2>
                         <hr/>
