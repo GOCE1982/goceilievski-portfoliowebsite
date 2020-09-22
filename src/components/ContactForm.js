1879602abf27316fc5b3d8f8e36d07613e03fa5c
GOCE1982 <goce.ilievski@live.com>
2020-09-22 12:07:50 +0200

    Netlify form fix attempt no2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,38 +12,29 @@ class ContactForm extends Component {
         this.state = {
             name: "",
             email: "",
-            message: "",
-            alert: null
+            message: ""
         }
     }
 
     handleSubmit = e => {
-        if((this.state.name === '' && this.state.email === '') || this.state.message === '') {
-            this.setState({alert: 'Please enter something'});
-            setTimeout(() => this.setState({alert: null}), 3000);
-        } else {
-            fetch('/contact', {
-                method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: encode({ "form-name": "contact", ...this.state })
-            })
-            .then(() => this.setState({alert: 'Message successfully sent'}))
-            .then(setTimeout(() => this.setState({alert: null}), 3000))
-            .catch(err => alert(err))
-        }
-
+        fetch("/contact", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
         e.preventDefault();
-    }
-
+      };
     handleChange = (e) => this.setState({ [e.target.name]: e.target.value} );
 
     render() {
-        const { name, email, message, alert } = this.state;
+        const { name, email, message } = this.state;
 
         return (
             <div>
                <h3>Send me a message</h3>
-                {alert !== null ? <p style={{color: '#2838dd', fontSize: '20px'}}>{`${alert}`}</p> : null}
                 <form name="contact" method="POST" data-netlify="true" onSubmit={this.handleSubmit}>
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
