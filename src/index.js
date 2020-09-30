7a42625d4b3421a64c45642c66359d827dd6e8da
GOCE1982 <goce.ilievski@live.com>
2020-09-30 18:56:04 +0200

    Fix broken links in Router

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import * as serviceWorker from './serviceWorker';
-import { Router } from 'react-router-dom';
+import { MemoryRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history} basename="/">
+  <Router 
+    history={history}
+    initialEntries={["/", "/resume", "/projects", "/contact"]}
+    initialIndex={0}
+  >
     <App />
   </Router>
   , document.getElementById('root')
